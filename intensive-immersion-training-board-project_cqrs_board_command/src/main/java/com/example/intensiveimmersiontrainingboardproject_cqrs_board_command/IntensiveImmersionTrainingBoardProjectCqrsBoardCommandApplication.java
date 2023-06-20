9671d86689f2b64c1d033952f5c1a3f97cedcf09
0Chord <seaweed.0chord@gmail.com>
2023-06-20 22:48:44 +0900

    feat : 추가 board-cqrs 패턴

---
+++ intensive-immersion-training-board-project_cqrs_board_command/src/main/java/com/example/intensiveimmersiontrainingboardproject_cqrs_board_command/IntensiveImmersionTrainingBoardProjectCqrsBoardCommandApplication.java
@@ -0,0 +1,13 @@
+package com.example.intensiveimmersiontrainingboardproject_cqrs_board_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensiveImmersionTrainingBoardProjectCqrsBoardCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensiveImmersionTrainingBoardProjectCqrsBoardCommandApplication.class, args);
+	}
+
+}
